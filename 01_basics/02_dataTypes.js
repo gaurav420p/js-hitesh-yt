c5c530636a668cc2f9c905a8f34f64a524322d22
GAURAV PATEL <118682748+gaurav420p@users.noreply.github.com>
2024-10-23 13:47:28 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,3 +3,11 @@
 // alert("hello ") we r using node js not brower. It will not run in node js
 
 console.log("23");
+
+let name="hitesh";
+let age=18;
+let isLoggedin=false;
+
+//number range => 2 to power 53 
+//big int
+//string
